b61817b79323f95d0474a212c4365dda875f6687
annnacaesar <Anechka22334@gmail.com>
2022-06-13 21:00:30 +0300

    Update API.js

--- src/js/js-partials/API.js
+++ src/js/js-partials/API.js
@@ -1,5 +1,5 @@
 const API_KEY = `7cb8097836a7a1f4e5c19953961668c8`;
-const BASE_URL = `https://api.themoviedb.org/3/`;
+const BASE_URL = `https://api.themoviedb.org/3`;
 
 // приклад запиту https://api.themoviedb.org/3/movie/550?api_key=7cb8097836a7a1f4e5c19953961668c8
 
