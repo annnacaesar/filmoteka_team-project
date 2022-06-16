3dca2e2bcb158d3eaec324d1738d3a061c1f9a99
yovbaks <yovbak.s@gmail.com>
2022-06-16 15:04:29 +0300

    modaaall
    Зробив динамічну модалку, але не можу її засунути в lightbox

--- src/js/js-partials/teamModal.js
+++ src/js/js-partials/teamModal.js
@@ -1,13 +1,15 @@
+import photoTsisar from "../../images/team/Tsisar.jpg";
+import photoTsapkov from "../../images/team/capkov.jpg";
+import photoOnishchuk from "../../images/team/onischuk.jpg";
+import photoBoyko from "../../images/team/boyko.jpg";
+import photoYovbak from "../../images/team/yovbak.jpg";
+import photoPopov from "../../images/team/popov.jpg";
+import photoBunio from "../../images/team/bunio.jpg";
+import photoChekov from "../../images/team/chekov.jpg";
 import * as basicLightbox from "basiclightbox";
 
-const modalTeamRef = document.querySelector('.footer__btn');
-
-modalTeamRef.addEventListener('click', onOpenModalTeam);
-
-function onOpenModalTeam() {
-    console.log('Hello');
-    const modalTeam = basicLightbox.create(`
-    <div class="modal__container">
+const modalTeamRef = document.querySelector(".footer__btn");
+const markup = ` <div class="modal__container">
     <button class="modal__close">
       <div class="modal__close-first"></div>
       <div class="modal__close-second"></div>
@@ -18,169 +20,212 @@ function onOpenModalTeam() {
         <ul class="team__list">
           <li class="team__item">
             <img
-              src="../../images/team/cisar-min.jpg"
+              src="${photoTsisar}"
               alt="Anna Tsisar"
               class="team__item-img"
              
             />
             <p class="team__item-name">Anna Tsisar</p>
             <p class="team__item-role">Team Lead</p>
-            <a href="http://github.com"
-              ><img
-                src="./images/team/github.png"
-                alt=""
-                width="20"
-                height="20"
-                class="team__item-icon"
-            /></a>
+            <div class="team__item-wrap"><a href="https://github.com/annnacaesar" target=_blank
+              >
+              <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-512.png" alt="" class="team__item-icon"></a>
+              <a href="https://www.linkedin.com/in/anna-tsisar-092b52223" target=_blank>
+              <img src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png" alt="" class="team__item-icon"></a>
+              </div>
           </li>
           <li class="team__item">
             <img
-              src="./images/team/capkov-min.jpg"
+              src="${photoTsapkov}"
               alt="Andriy Tsapkov"
               class="team__item-img"
              
             />
             <p class="team__item-name">Andriy Tsapkov</p>
             <p class="team__item-role">Scrum</p>
-            <a href="http://github.com"
-              ><img
-                src="./images/team/github.png"
-                alt=""
-                width="20"
-                height="20"
-                class="team__item-icon"
-            /></a>
+            <div class="team__item-wrap"><a href="https://github.com/AndreyTsapkov" target=_blank
+              >
+              <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-512.png" alt="" class="team__item-icon"></a>
+              <a href="https://www.linkedin.com/in/andrey-tsapkov-996aa019a" target=_blank>
+              <img src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png" alt="" class="team__item-icon"></a>
+              </div>
           </li>
           <li class="team__item">
             <img
-              src="./images/team/dima-min.jpg"
+              src="${photoOnishchuk}"
               alt="Dmytro Onishchuk"
               class="team__item-img"
            
             />
             <p class="team__item-name">Dmytro Onishchuk</p>
             <p class="team__item-role">Developer</p>
-            <a href="http://github.com"
-              ><img
-                src="./images/team/github.png"
-                alt=""
-                width="20"
-                height="20"
-                class="team__item-icon"
-            /></a>
+            <div class="team__item-wrap"><a href="http://github.com/dpartner" target=_blank
+              >
+              <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-512.png" alt="" class="team__item-icon"></a>
+              <a href="https://www.linkedin.com/in/%D0%B4%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9-%D0%BE%D0%BD%D0%B8%D1%89%D1%83%D0%BA-60103118a/" target=_blank>
+              <img src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png" alt="" class="team__item-icon"></a>
+              </div>
           </li>
           <li class="team__item">
             <img
-              src="./images/team/boyko-min.jpg"
+              src="${photoBoyko}"
               alt="Natalia Boyko"
               class="team__item-img"
             
             />
             <p class="team__item-name">Natalia Boyko</p>
             <p class="team__item-role">Developer</p>
-            <a href="http://github.com"
-              ><img
-                src="./images/team/github.png"
-                alt=""
-                width="20"
-                height="20"
-                class="team__item-icon"
-            /></a>
+            <div class="team__item-wrap"><a href="http://github.com/NatalaBoiko" target=_blank
+              >
+              <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-512.png" alt="" class="team__item-icon"></a>
+              <a href="https://www.linkedin.com/in/nataliia-boiko-305140228" target=_blank>
+              <img src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png" alt="" class="team__item-icon"></a>
+              </div>
           </li>
           <li class="team__item">
             <img
-              src="./images/team/yovbak-min.jpg"
+              src="${photoYovbak}"
               alt="Serhiy Yovbak"
               class="team__item-img"
             
             />
             <p class="team__item-name">Serhiy Yovbak</p>
             <p class="team__item-role">Developer</p>
-            <a href="http://github.com"
-              ><img
-                src="./images/team/github.png"
-                alt=""
-                width="20"
-                height="20"
-                class="team__item-icon"
-            /></a>
+            <div class="team__item-wrap"><a href="https://github.com/yovbaks" target=_blank
+              >
+              <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-512.png" alt="" class="team__item-icon"></a>
+              <a href="https://www.linkedin.com/in/serhiy-yovbak-4a778879" target=_blank>
+              <img src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png" alt="" class="team__item-icon"></a>
+              </div>
           </li>
           <li class="team__item">
             <img
-              src="./images/team/timur-min.jpg"
+              src="${photoPopov}"
               alt="Tymur Popov"
               class="team__item-img"
            
             />
             <p class="team__item-name">Tymur Popov</p>
             <p class="team__item-role">Developer</p>
-            <a href="http://github.com"
-              ><img
-                src="./images/team/github.png"
-                alt=""
-                width="20"
-                height="20"
-                class="team__item-icon"
-            /></a>
+            <div class="team__item-wrap"><a href="http://github.com/Tymur-Popov" target=_blank
+              >
+              <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-512.png" alt="" class="team__item-icon"></a>
+              <a href="https://www.linkedin.com" target=_blank>
+              <img src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png" alt="" class="team__item-icon"></a>
+              </div>
           </li>
           <li class="team__item">
             <img
-              src="./images/team/bunio-min.jpg"
+              src="${photoBunio}"
               alt="Oleg Bunio"
               class="team__item-img"
           
             />
             <p class="team__item-name">Oleg Bunio</p>
             <p class="team__item-role">Developer</p>
-            <a href="http://github.com"
-              ><img
-                src="./images/team/github.png"
-                alt=""
-                width="20"
-                height="20"
-                class="team__item-icon"
-            /></a>
+            <div class="team__item-wrap"><a href="https://github.com/BunoOleg" target=_blank
+              >
+              <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-512.png" alt="" class="team__item-icon"></a>
+              <a href="https://www.linkedin.com/in/%D0%BE%D0%BB%D0%B5%D0%B3-%D0%B1%D1%83%D0%BD%D1%8C%D0%BE-a63a97241" target=_blank>
+              <img src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png" alt="" class="team__item-icon"></a>
+              </div>
           </li>
           </li>
             <li class="team__item">
             <img
-              src="./images/team/alex-min.jpg"
+              src="${photoChekov}"
               alt="Alex Chekov"
               class="team__item-img"
            
             />
             <p class="team__item-name">Alex Chekov</p>
             <p class="team__item-role">Developer</p>
-            <a href="http://github.com"
-              ><img
-                src="./images/team/github.png"
-                alt=""
-                width="20"
-                height="20"
-                class="team__item-icon"
-            /></a>
+           <div class="team__item-wrap"><a href="https://github.com/AChekov" target=_blank
+              >
+              <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-512.png" alt="" class="team__item-icon"></a>
+              <a href="https://www.linkedin.com/in/alexey-chekov" target=_blank>
+              <img src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png" alt="" class="team__item-icon"></a>
+              </div>
           </li>
                     
         </ul>
       </div>
     </div>
   </div>
-`,{
-        onShow: modalTeam => {
-          window.addEventListener("keydown", escapeKeyCloseModal);
-          modalTeam.element().querySelector(".modal__close").onclick = modalTeam.close;
-        },
-        onClose: modalTeam => {
-          window.removeEventListener("keydown", escapeKeyCloseModal);
-        },
-      }
-    );
-    function escapeKeyCloseModal(event) {
-      if (event.code === "Escape") {
-        modalTeam.close();
-      }
+`;
+
+modalTeamRef.addEventListener("click", onOpenModalTeam);
+
+
+function onOpenModalTeam() {
+  console.log("Hello");
+  const modalTeam = basicLightbox.create(markup, {
+    onShow: modalTeam => {
+      window.addEventListener("keydown", escapeKeyCloseModal);
+      modalTeam.element().querySelector(".modal__close").onclick =
+        modalTeam.close;
+    },
+    onClose: modalTeam => {
+      window.removeEventListener("keydown", escapeKeyCloseModal);
+    },
+  });
+  function escapeKeyCloseModal(event) {
+    if (event.code === "Escape") {
+      modalTeam.close();
     }
-    modalTeam.show();
   }
-    
+  modalTeam.show();
+}
+
+// ======================================================================
+import teamList from './teamList';
+import teamTpl from "../../templates/team.hbs";
+
+// const openTeamModalBtn = document.querySelector('.footer__btn');
+const teamContainer = document.querySelector('.team__list');
+
+
+// const closeTeamModalBtn= document.querySelector('.js-team-close');
+// const backdropTeamModal = document.querySelector('.js-backdrop');
+
+// const body = document.querySelector('body');
+
+openTeamModalBtn.addEventListener('click', onOpenModal);
+const mmarkup = renderTeam(teamList);
+function onOpenModal(evt) {
+    evt.preventDefault();
+    console.log('Hello');
+
+  renderTeam(teamList);
+  // window.addEventListener('keydown', onEscKeyPress);
+  // closeTeamModalBtn.addEventListener('click', onCloseModal);
+  // backdropTeamModal.addEventListener('click', onBackdropClick);
+  // body.classList.add('popup-open');
+  // body.classList.add('show-modal');
+}
+
+function renderTeam() {
+  const markup = teamTpl(teamList);
+  teamContainer.innerHTML = markup;
+}
+
+// function onCloseModal() {
+//   window.removeEventListener('keydown', onEscKeyPress);
+//   document.body.classList.remove('show-modal');
+//   document.body.classList.remove('popup-open');
+//   closeTeamModalBtn.removeEventListener('click', onCloseModal);
+//   backdropTeamModal.removeEventListener('click', onBackdropClick);
+//   teamContainer.innerHTML = "";
+// }
+
+// function onBackdropClick(event) {
+//   if (event.currentTarget === event.target) {
+//     onCloseModal();
+//   }
+// }
+
+// function onEscKeyPress(event) {
+//   if (event.code === 'Escape') {
+//     onCloseModal();
+//   }
+// }
