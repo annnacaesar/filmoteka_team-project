2945c469782fbc5b96ce0133258e898cd6161bb1
Andrey Tsapkov <tsapkov1994@gmail.com>
2022-06-17 02:46:33 +0300

    pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,7 +1,15 @@
 // fetch(
 //   `https://api.themoviedb.org/3/movie/550?api_key=7cb8097836a7a1f4e5c19953961668c8`
 // ).then(response => response.json());
-const pageButtons = document.querySelector("button[aria-label='next-page'");
+const refs = {
+  arrowLeft: document.querySelector("button[aria-label='previous-page'"),
+  arrowRight: document.querySelector("button[aria-label='next-page'"),
+  pagination: document.querySelector(".pagination"),
+  firstButton: document.querySelector(".firstButton-and-threeDots"),
+  lastButton: document.querySelector(".lastButton-and-threeDots"),
+};
+let pageNumber = 1;
+// const pageButtons = document.querySelector("button[aria-label='next-page'");
 // function getNumberOfPages(data) {
 //   const numberOfPages = data.totalPages;
 //   return numberOfPages;
@@ -20,17 +28,72 @@ const pageButtons = document.querySelector("button[aria-label='next-page'");
 //   button.classList.add("visually-hidden");
 // });
 
-function renderButtons(pageNumber) {
-  const screenWidth = window.innerWidth;
-  console.log(screenWidth);
-  if (screenWidth < 768) {
-    let numberOfPages = pageNumber + 4;
-  } else {
-    let numberOfPages = pageNumber + 6;
+function renderButtons() {
+  let beforeActivePage = pageNumber - 2;
+  let afterActivePage = pageNumber + 2;
+  if (beforeActivePage < 3) {
+    beforeActivePage = 1;
+    afterActivePage = 5;
   }
-  for (let i = pageNumber; i <= numberOfPages; i += 1) {
-    const pageButton = `<button data-page="${i}" class="button-page">${i}</button>`;
-    pageButtons.insertAdjacentHTML("beforebegin", pageButton);
+  for (let i = beforeActivePage; i <= afterActivePage; i += 1) {
+    if (i === pageNumber) {
+      const pageButton = `<button id="paginationButton" data-page="${i}" class="button-page paginationButton active">${i}</button>`;
+      refs.pagination.insertAdjacentHTML("beforeend", pageButton);
+    } else {
+      const pageButton = `<button id="paginationButton" data-page="${i}" class="button-page paginationButton">${i}</button>`;
+      refs.pagination.insertAdjacentHTML("beforeend", pageButton);
+    }
+  }
+
+  if (beforeActivePage >= 3) {
+    if (isThreeDots()) {
+      return;
+    } else {
+      const firstButton = `<button id="paginationButton" data-page="1" class="button-page paginationButton">1</button>`;
+      const threeDots = `<span class='page-buttons__points'>···</span>`;
+      refs.firstButton.insertAdjacentHTML("beforeend", firstButton);
+      refs.firstButton.insertAdjacentHTML("beforeend", threeDots);
+    }
+  }
+  if (beforeActivePage < 3 && isThreeDots()) {
+    refs.firstButton.innerHTML = "";
   }
 }
-renderButtons(2);
+
+function onClickArrowLeft() {
+  if (pageNumber === 1) {
+    return;
+  }
+  pageNumber -= 1;
+  resetButtons();
+  renderButtons();
+}
+function onClickArrowRight() {
+  pageNumber += 1;
+  resetButtons();
+  renderButtons();
+}
+
+function resetButtons() {
+  refs.pagination.innerHTML = "";
+}
+
+function onClickButton(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) return;
+  pageNumber = Number(event.target.dataset.page);
+  resetButtons();
+  renderButtons();
+}
+
+function isThreeDots() {
+  const isThreeDots = document.querySelector(".page-buttons__points");
+  if (isThreeDots === null) {
+    return false;
+  }
+  return true;
+}
+renderButtons();
+refs.arrowLeft.addEventListener("click", onClickArrowLeft);
+refs.arrowRight.addEventListener("click", onClickArrowRight);
+refs.pagination.addEventListener("click", onClickButton);
