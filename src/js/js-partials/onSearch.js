02b4e2ba1ebb2cfae0b8cdcc6a5815a26c68544b
NatalaBoiko <natalabojko@gmail.com>
2022-06-17 19:05:34 +0300

    -shadows, serchQery +-

--- src/js/js-partials/onSearch.js
+++ src/js/js-partials/onSearch.js
@@ -1,4 +1,3 @@
-
 import ApiService from "./API";
 import appendFilmsMarkup from "./appendFilmsMarkup";
 import clearFilmsContainer from "./clearFilmsContainer";
@@ -7,8 +6,22 @@ const apiService = new ApiService();
 
 const searchForm = document.querySelector(".input__wraper");
 const filmsContainerIndex = document.querySelector(".js-films-list-index");
+const inputError = document.querySelector(".input__error");
+console.log(inputError);
 
 searchForm.addEventListener("submit", onSearch);
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   apiService.query = e.currentTarget.elements.searchQuery.value;
+//   apiService.resetPage();
+
+//   apiService.fetchMoviesySearch().then(({ results }) => {
+//     clearFilmsContainer();
+//     appendFilmsMarkup(results, filmsContainerIndex);
+//   });
+// }
 
 function onSearch(e) {
   e.preventDefault();
@@ -16,9 +29,18 @@ function onSearch(e) {
   apiService.query = e.currentTarget.elements.searchQuery.value;
   apiService.resetPage();
 
-  apiService.fetchMoviesySearch().then(({ results }) => {
-    clearFilmsContainer();
-    appendFilmsMarkup(results,filmsContainerIndex);
-  });
-
+  if (apiService.query.length === 0) {
+    inputError.textContent =
+      "Search result not successful. Enter the correct movie name and ";
+    setTimeout(() => {
+      document.location.reload();
+    }, 2000);
+    return;
+  } else {
+    inputError.textContent = " ";
+    apiService.fetchMoviesySearch().then(({ results }) => {
+      clearFilmsContainer();
+      appendFilmsMarkup(results, filmsContainerIndex);
+    });
+  }
 }
