28233b273768d3122c2aca270dc466fe9ab89566
annnacaesar <Anechka22334@gmail.com>
2022-06-19 22:23:10 +0300

    1

---
+++ src/js/js-partials/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3mAeoJSkVgXp7W5wSmHtE9e1wFGsySmc",
+  authDomain: "filmoteka-209ce.firebaseapp.com",
+  projectId: "filmoteka-209ce",
+  storageBucket: "filmoteka-209ce.appspot.com",
+  messagingSenderId: "68393581409",
+  appId: "1:68393581409:web:5521fb7cea0b94cd42e320"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
