80c816787777cafef07af03e82a024d8cb10e0a3
annnacaesar <Anechka22334@gmail.com>
2022-06-20 13:54:58 +0300

    Update firebase.js

--- src/js/js-partials/firebase.js
+++ src/js/js-partials/firebase.js
@@ -15,3 +15,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export class Firebase  {
+  static craete(film) {
+    return fetch('https://filmoteka-209ce-default-rtdb.firebaseio.com/films.json', {
+      method: "POST",
+      body: JSON.stringify(film),
+      header: {
+        'Content-Type' : 'application/json',
+      }
+    }).then(response => response.json()).then(response => console.log(response))
+  }
+}
